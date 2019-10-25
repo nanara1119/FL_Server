36a8774ab86850594e61bc60406d6747fe8f4c2d
Nanara <nanara1119@gmail.com>
2019-10-25 18:01:12 +0900

    1. ndarray serializable error ...

--- app/Federated.py
+++ app/Federated.py
@@ -1,6 +1,9 @@
+import numpy as np
+
+
 class FederatedServer:
     max_count = 10
-    global_weight = []
+    global_weight = None
     local_weights = []
     current_count = 0
 
@@ -17,7 +20,17 @@ class FederatedServer:
 
     @classmethod
     def avg(cls):
-        print("averaging : {}".format(cls.local_weights))
+        print("averaging : {} /// {}".format(cls.local_weights, cls.global_weight))
+
+        if cls.global_weight is None:
+            cls.global_weight = cls.local_weights.pop(0)
+
+        for i in range(len(cls.local_weights)):
+            for j in range(len(cls.local_weights[i])):
+                cls.global_weight[j] += cls.local_weights[i][j]
+        cls.global_weight = np.divide(cls.global_weight, 10)
+        cls.local_weights = []
+        print("avg : {}".format(cls.global_weight))
 
     @classmethod
     def get_avg(cls):
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,14 +14,21 @@ from app.Federated import FederatedServer
 @api_view(['GET', 'PUT'])
 def weight(request):
     if request.method == 'GET':
-        print("request GET weight : ")
-        FederatedServer.get_avg()
-        return HttpResponse(FederatedServer.get_avg(), status.HTTP_200_OK)
+
+        '''
+        global_weight = FederatedServer.get_avg()
+        if len(global_weight) == 0:
+            global_weight = None
+        '''
+        global_weight = json.dumps(FederatedServer.get_avg())
+
+        print("request get weight : {}".format(global_weight))
+        return HttpResponse(global_weight, status.HTTP_200_OK)
 
     elif request.method == 'PUT':
         print("request PUT weight")
         data = JSONParser().parse(request)
-        print("PUT data : {}".format(data))
+        print("PUT data : {}, {}".format(type(data), len(data)))
         FederatedServer.update(data)
         return HttpResponse("Request PUT OK", status.HTTP_200_OK)
 
