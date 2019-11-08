8779a27d32decd6ec02e12ed478de9055d299d9a
Lee GeunHyeong <nanara1119@gmail.com>
2019-11-08 15:38:14 +0900

    no message

--- app/Federated.py
+++ app/Federated.py
@@ -12,39 +12,41 @@ class FederatedServer:
     current_count = 0
     current_round = 0
 
+    def __init__(self):
+        print("Federated init")
+
     @classmethod
     def update(cls, local_weight):
-        local_weights = copy.deepcopy(local_weight)
+        local_avg = copy.deepcopy(local_weight)
         cls.current_count += 1
 
-        if cls.current_count % cls.max_count == 0:
+        cls.local_weights.append(local_avg)
+
+        print("update count : {}, {}".format(cls.current_count, len(cls.local_weights)))
+
+        if cls.current_count == cls.max_count:
+            cls.avg()
             cls.current_count = 0
             cls.current_round += 1
-            cls.avg()
-        else:
-            #temp = np.array(local_weights, dtype=np.float32)
-            cls.local_weights.append(local_weights)
-
-        #logger.info("update round : {}, count: {}".format(cls.current_round, cls.current_count))
-        #print("update count : {}".format(cls.current_count))
 
     @classmethod
     def avg(cls):
 
         if cls.global_weight is None:
-            #cls.global_weight = cls.local_weights[0]
-            cls.global_weight = cls.local_weights.pop(0)
+            print("avg None")
+            cls.global_weight = cls.local_weights[0]
 
-        #print("temp : {}".format(len(cls.local_weights)))
+        else:
+            print("temp : {}".format(len(cls.local_weights)))
+            for i in range(len(cls.local_weights)):
+                for j in range(len(cls.local_weights[i])):
+                    temp = np.array(cls.local_weights[i][j], dtype=np.float32)
+                    cls.global_weight[j] += temp
 
-        for i in range(len(cls.local_weights)):
-            for j in range(len(cls.local_weights[i])):
-                temp = np.array(cls.local_weights[i][j], dtype=np.float64)
-                cls.global_weight[j] += temp
+            cls.global_weight = np.divide(cls.global_weight, len(cls.local_weights)) #   평균 계산
 
-        cls.global_weight = np.divide(cls.global_weight, len(cls.local_weights)) #   평균 계산
-        cls.local_weights = []  #   다음 라운드를 위해 이전의 local weight 리스트 삭제
-        print("global weight update")
+        cls.local_weights = []  # 다음 라운드를 위해 이전의 local weight 리스트 삭제
+        print("global weight update : global round : {}".format(cls.current_round))
 
     @classmethod
     def get_avg(cls):
